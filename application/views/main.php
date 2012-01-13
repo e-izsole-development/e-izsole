a165167c9082c69704ccb76a549479f4356efd1b
Janis Kivitis <kimanx@inbox.lv>
2012-01-14 00:17:56 +0200

    +Css и всякая херня, возможно..

--- application/views/main.php
+++ application/views/main.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>e-izsole</title>
-        <LINK REL=StyleSheet HREF="main.css" TYPE="text/css"></link>
+        <LINK REL=StyleSheet HREF="application/views/main.css" TYPE="text/css"></link>
 <body>
     <div id="TopBar" style="display: block; background: grey; ">
         <span id="forRegisteredUser">
@@ -24,18 +24,31 @@
         <button style="float:right;">lang</button>
         <button style="float:right;">curr</button>
 	</div>
-    <span style="width:auto; background:#9999ff; margin:20% 20%; height:100%;" >
+    <span id="testzone" style="width:400px; background:white; margin:20% 20%; height:100%;" >
         
 	<div id="Menu" style="display: block; ">
-		Menu
+		<p>Menu</p>
                 
 	</div>
 	<div id="Kategorijas" style="display: inline; ">
-		kategorijas
+		<p>kategorijas</p>
 	</div>
 	<div id="Main" style="display: inline; ">
-		main
+		<p>main</p>
 	</div>
+        
     </span>
+    <div id="comixzone">
+            
+        <h1>Don't worry it's just a test !!!</h1>
+        <h2>If you think, that this is h2, than you better know- it's a lie!! You can check it</h2>
+        <h3>But this sure iss h3 !</h3>
+        
+        <p>paragraph tag is here with purpose</p>
+        <pre>as well as pre
+        second line of pre
+        third line of pre</pre>
+            
+        </div>
 </body>
 </html>
