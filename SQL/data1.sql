1e1b6e15e03496e1179ebbff3eeada7e554bd6d4
vitalijs <vitalij.gagunov@inbox.lv>
2012-01-14 03:42:42 +0200

    DataBase creation, and ading some data scripts

---
+++ SQL/data1.sql
@@ -0,0 +1,66 @@
+USE `e-izsole`;
+
+INSERT INTO dbo_users (`id`,`name`,`surname`,`country`,`city`,`adress`,`phone_number`,`mobile_operator`,`e_mail`,`username`,`password`,`user_type`,`currency`,`language`) VALUES
+(1,'Vitalijs','Flinstouns','Latvija','Daugavpils','Energetiku 5 - 6','+37128650347',1,'worm333@inbox.lv','vita1ij','gfhjkm123','a',2,2),
+(2,'Janis','Petrovs','Latvija','Riga','Zellu 5-56','+37128311093',1,'kimanx@inbox.lv','kimanx','qwerty','m',2,2),
+(3,'Katerina','Maliseva','Lietuva','Visaginas','Pienas 65','+37128237862',2,'katerinkamardusevish@inbox.lv','kitkat','123','r',2,2),
+(4,'Tatjana','Kastaneda','Igaunija','Tartu','ulica 4 - 5','+37239574374',1,'zev333@gmail.com','zev','zev123','r',2,2),
+(5,'Vladislavs','Gruzis','Latvija','Liepaja','Rigas 66 - 6','+37256774374',2,'asd@foo.ba','qwe','asd','r',1,1),
+(6,'Oskars','Pigulis','Lietuva','Vilnius','Miera 54-2','+37239574234',2,'foo@bar.com','jkdfh','qwef','r',1,2),
+(7,'Edgars','Stucka','Latvija','Riga','Daugavpils 4-4','+37188476333',2,'booo@lin.lv','boogaga','dqwjh23','m',2,1),
+(8,'Toms','Caks','Latvija','Riga','Elizabetes 31-3','+37123434688',1,'nemo@inbox.com','nemo','1q2w3e3r4','r',1,2),
+(9,'Aleksandrs','Berzins','Igaunija','Tartu','Ziieduu 6-5','+37298978765',0,'kim@gagarin.lv','gagarin','w##w','r',1,2),
+(10,'Oskars','Kivi','Latvija','Daugavpils','Smilsu 65 - 7','+37122345678',1,'ggg@gmail.com','fiba','oijqw','r',2,2),
+(11,'Arnolds','Lacuks','Latvija','Ventspils','Marijas 4-5','+37121345876',1,'sudk@gmail.com','sudoku','q@w@q','r',2,2);
+
+INSERT INTO dbo_categories(`id`,`title`,`language`) VALUES
+(1,'jewerly',2),
+(2,'phones',2),
+(3,'cars',2),
+(4,'electronics',2),
+(5,'toys',2),
+(6,'books',2),
+(7,'clothes',2),
+(8,'art',2);
+
+INSERT INTO dbo_parameters(`id`,`category`,`title`,`language`) VALUES
+(1,2,'Model',2),
+(2,2,'Weight',2),
+(3,2,'Has WiFi',2),
+(4,2,'Has Bluetooth',2),
+(5,2,'Color',2),
+(6,6,'Author',2),
+(7,6,'Title',2),
+(8,6,'Pages',2),
+(9,6,'Hard Cover',2);
+
+INSERT INTO dbo_items (`id`,`seller_id`,`title`,`Auction`,`price`,`category`,`winner`) VALUES
+(1,1,'War of the Worlds',1,3,6,0),
+(2,2,'Roses are red, Violets are blue',0,1,6,0),
+(3,3,'Story about 3 pigs',0,1,6,0),
+(4,4,'Nokia 3310',0,9,2,0),
+(5,5,'Nokia 5800',1,10,2,0);
+
+INSERT INTO dbo_parameter_values (`id`,`parameter`,`value`,`item_id`,`language`) VALUES
+(1,1,'Herbert Wells',1,2),
+(2,2,'War of the worlds',1,2),
+(3,3,'500',1,2),
+(4,4,'yes',1,2),
+(5,1,'Unknown',2,2),
+(6,2,'Roses are red, Violets are blue',2,2),
+(7,3,'200',2,2),
+(8,4,'No',2,2),
+(9,1,'People',3,2),
+(10,2,'Story about 3 pigs',3,2),
+(11,3,'10',3,2),
+(12,4,'No',3,2),
+(13,1,'Nokia 3310',4,2),
+(14,2,'150g',4,2),
+(15,3,'No',4,2),
+(16,4,'No',4,2),
+(17,5,'Red',4,2),
+(18,1,'Nokia 5800',5,2),
+(19,2,'160g',5,2),
+(20,3,'Yes',5,2),
+(21,4,'Yes',5,2),
+(22,5,'black',5,2);
---
+++ SQL/e-izsole.sql
@@ -0,0 +1,134 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+CREATE DATABASE `e-izsole` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `e-izsole`;
+
+CREATE TABLE IF NOT EXISTS `dbo_users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_latvian_ci NOT NULL,
+  `surname` varchar(255) COLLATE utf8_latvian_ci NOT NULL,
+  `country` varchar(255) COLLATE utf8_latvian_ci NOT NULL,
+  `city` varchar(255) COLLATE utf8_latvian_ci NOT NULL,
+  `adress` varchar(255) COLLATE utf8_latvian_ci NOT NULL,
+  `phone_number` varchar(255) COLLATE utf8_latvian_ci,
+  `mobile_operator` int,
+  `e_mail` varchar(255) COLLATE utf8_latvian_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8_latvian_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_latvian_ci NOT NULL,
+  `user_type` char NOT NULL,
+  `currency` int NOT NULL,
+  `language` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=3 ;
+
+CREATE TABLE IF NOT EXISTS `dbo_deals` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`buyer_id` INT NOT NULL,
+	`seller_id` INT NOT NULL,
+	`item_id` INT NOT NULL,
+	`deal_date` TEXT NOT NULL,
+	`deal_status` boolean NOT NULL,
+	PRIMARY KEY (`id`)
+)ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=3 ;
+
+CREATE TABLE IF NOT EXISTS `dbo_recently_watched` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`user_id` int not null,
+	`item_id` INT NOT NULL,
+	`date` TEXT NOT NULL,
+	PRIMARY KEY (`id`)
+)ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=3 ;
+
+CREATE TABLE IF NOT EXISTS `dbo_tags` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`title` varchar(125) NOT NULL,
+	PRIMARY KEY (`id`)
+)ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=3 ;
+
+CREATE TABLE IF NOT EXISTS `dbo_items` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`seller_id` INT NOT NULL,
+	`title` varchar(255) NOT NULL,
+	`photo` BLOB ,
+	`Auction` boolean NOT NULL,
+	`price` INT NOT NULL,
+	`category` INT NOT NULL,
+	`winner` INT NOT NULL,
+	PRIMARY KEY (`id`)
+)ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=3 ;
+
+CREATE TABLE IF NOT EXISTS `dbo_mobile_operators` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`title` VARCHAR(64) NOT NULL,
+	PRIMARY KEY (`id`)
+)ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=3 ;
+
+CREATE TABLE IF NOT EXISTS `dbo_languages` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`title` VARCHAR(63) NOT NULL,
+	`course` REAL,
+	PRIMARY KEY (`id`)
+)ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=3 ;
+
+CREATE TABLE IF NOT EXISTS `dbo_parameter_values` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`Parameter` INT NOT NULL,
+	`value` TEXT NOT NULL,
+	`item_id` INT NOT NULL,
+	`language` INT NOT NULL,
+	PRIMARY KEY (`id`)
+)ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=3 ;
+
+CREATE TABLE IF NOT EXISTS `dbo_parameters` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`category` INT NOT NULL,
+	`title` VARCHAR(255) NOT NULL,
+	`language` INT NOT NULL,
+	PRIMARY KEY (`id`)
+)ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=3 ;
+
+CREATE TABLE IF NOT EXISTS `dbo_categories` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`title` VARCHAR(255) NOT NULL,
+	`language` INT NOT NULL,
+	PRIMARY KEY (`id`)
+)ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=3 ;
+
+CREATE TABLE IF NOT EXISTS `dbo_user_items` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`user` INT NOT NULL,
+	`item` INT NOT NULL,
+	PRIMARY KEY (`id`)
+)ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=3 ;
+
+CREATE TABLE IF NOT EXISTS `dbo_languages` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`title` VARCHAR(127) NOT NULL,
+	PRIMARY KEY (`id`)
+)ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=3 ;
+
+CREATE TABLE IF NOT EXISTS `dbo_item_description` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`user` INT NOT NULL,
+	`item` INT NOT NULL,
+	PRIMARY KEY (`id`)
+)ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=3 ;
+
+INSERT INTO `dbo_mobile_operators` (`id`, `title`) VALUES
+(1,'LMT'),
+(2, 'Tele 2');
+
+INSERT INTO `dbo_languages` (`id`, `title`) VALUES
+(1,'LV'),
+(2,'EN'),
+(3,'RU');
+
+
+
+CREATE USER 'eizsoleuser'@'localhost' IDENTIFIED BY  'eipass123';
+
+GRANT USAGE ON * . * TO  'eizsoleuser'@'localhost' 
+    IDENTIFIED BY  'eipass123' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
+
+GRANT ALL PRIVILEGES ON  `e-izsole` . * TO  'eizsoleuser'@'localhost' WITH GRANT OPTION ;
+
